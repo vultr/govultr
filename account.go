30fde8e982a96817d43a2c9b922831d52a58b90f
David Dymko <dymkod@gmail.com>
2019-04-22 14:07:31 -0400

    godoc comments for account

--- account.go
+++ account.go
@@ -5,14 +5,18 @@ import (
 	"net/http"
 )
 
+// AccountService is the interface to interact with Accounts endpoint on the Vultr API
+// Link: https://www.vultr.com/api/#account
 type AccountService interface {
 	GetInfo(ctx context.Context) (*Account, error)
 }
 
+// AccountServiceHandler handles interaction with the account methods for the Vultr API
 type AccountServiceHandler struct {
 	client *Client
 }
 
+// Account represents a Vultr account
 type Account struct {
 	Balance           string `json:"balance"`
 	PendingCharges    string `json:"pending_charges"`
@@ -20,6 +24,7 @@ type Account struct {
 	LastPaymentAmount string `json:"last_payment_amount"`
 }
 
+// Get Vultr account info
 func (a *AccountServiceHandler) GetInfo(ctx context.Context) (*Account, error) {
 
 	uri := "/v1/account/info"
