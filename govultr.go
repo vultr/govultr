2c91259d9705bc2a4e08d804ee584a2795948d9e
David Dymko <dymkod@gmail.com>
2019-04-22 14:07:08 -0400

    added ability to override client struct + godoc comments

--- govultr.go
+++ govultr.go
@@ -9,12 +9,14 @@ import (
 	"net/http"
 	"net/url"
 	"strings"
+	"time"
 )
 
 const (
 	version     = "0.0.1"
 	DefaultBase = "https://api.vultr.com"
 	UserAgent   = "govultr/" + version
+	RateLimit   = 200 * time.Millisecond
 )
 
 // ApiKey contains a users API Key for interacting with the API
@@ -37,11 +39,25 @@ type Client struct {
 	// API Key
 	ApiKey ApiKey
 
+	// API Rate Limit - Vultr rate limits based on time
+	RateLimit time.Duration
+
 	// Services used to interact with the API
 	Account AccountService
+
+	// Optional function called after every successful request made to the Vultr API
+	onRequestCompleted RequestCompletionCallback
 }
 
+// RequestCompletionCallback defines the type of the request callback function
+type RequestCompletionCallback func(*http.Request, *http.Response)
+
+// New Client returns a Vultr API Client
 func NewClient(httpClient *http.Client, key string) *Client {
+
+	if httpClient == nil {
+		httpClient = http.DefaultClient
+	}
 
 	baseUrl, _ := url.Parse(DefaultBase)
 
@@ -49,6 +65,7 @@ func NewClient(httpClient *http.Client, key string) *Client {
 		client:    httpClient,
 		BaseUrl:   baseUrl,
 		UserAgent: UserAgent,
+		RateLimit: RateLimit,
 	}
 
 	client.Account = &AccountServiceHandler{client}
@@ -59,6 +76,7 @@ func NewClient(httpClient *http.Client, key string) *Client {
 	return client
 }
 
+// NewRequest creates an API Request
 func (c *Client) NewRequest(ctx context.Context, method, uri string, body url.Values) (*http.Request, error) {
 
 	path, err := url.Parse(uri)
@@ -94,12 +112,20 @@ func (c *Client) NewRequest(ctx context.Context, method, uri string, body url.Va
 	return req, nil
 }
 
-//todo DoWithContext
+// DoWithContext sends an API Request and returns back the response. The API response is checked  to see if it was
+// a successful call. A successful call is then checked to see if we need to unmarshal since some resources
+// have their own implements of unmarshal.
 func (c *Client) DoWithContext(ctx context.Context, r *http.Request, data interface{}) error {
 
-	// todo slow this down with a sleep for the rate limit
+	// Sleep this call
+	time.Sleep(c.RateLimit)
+
 	req := r.WithContext(ctx)
 	res, err := c.client.Do(req)
+
+	if c.onRequestCompleted != nil {
+		c.onRequestCompleted(req, res)
+	}
 
 	//todo handle the error this might throw
 	defer res.Body.Close()
@@ -128,4 +154,26 @@ func (c *Client) DoWithContext(ctx context.Context, r *http.Request, data interf
 	}
 
 	return errors.New(string(body))
+}
+
+// Overrides the default BaseUrl
+func (c *Client) SetBaseUrl(baseUrl string) error {
+	updatedUrl, err := url.Parse(baseUrl)
+
+	if err != nil {
+		return err
+	}
+
+	c.BaseUrl = updatedUrl
+	return nil
+}
+
+// Overrides the default rateLimit
+func (c *Client) SetRateLimit(time time.Duration) {
+	c.RateLimit = time
+}
+
+// OnRequestCompleted sets the API request completion callback
+func (c *Client) OnRequestCompleted(rc RequestCompletionCallback) {
+	c.onRequestCompleted = rc
 }
