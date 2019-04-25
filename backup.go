dec138ddbcbd429bb8372340760efa2dd93429a7
Fady Farid <ffarid@vultr.com>
2019-04-25 10:09:32 -0400

    add Backup.Get and Backup.GetListBySub

--- backup.go
+++ backup.go
@@ -9,6 +9,8 @@ import (
 // Link: https://www.vultr.com/api/#backup
 type BackupService interface {
 	GetList(ctx context.Context) ([]Backup, error)
+	Get(ctx context.Context, backupID string) (*Backup, error)
+	GetListBySub(ctx context.Context, subID string) ([]Backup, error)
 }
 
 // BackupServiceHandler handles interaction with the backup methods for the Vultr API
@@ -35,7 +37,60 @@ func (b *BackupServiceHandler) GetList(ctx context.Context) ([]Backup, error) {
 	}
 
 	backupsMap := make(map[string]Backup)
+	err = b.client.DoWithContext(ctx, req, &backupsMap)
+	if err != nil {
+		return nil, err
+	}
 
+	var backups []Backup
+	for _, backup := range backupsMap {
+		backups = append(backups, backup)
+	}
+
+	return backups, nil
+}
+
+// Get retrieves a backup that matches the given backupID
+func (b *BackupServiceHandler) Get(ctx context.Context, backupID string) (*Backup, error) {
+	uri := "/v1/backup/list"
+	req, err := b.client.NewRequest(ctx, http.MethodGet, uri, nil)
+
+	if err != nil {
+		return nil, err
+	}
+
+	q := req.URL.Query()
+	q.Add("BACKUPID", backupID)
+	req.URL.RawQuery = q.Encode()
+
+	backupsMap := make(map[string]Backup)
+	err = b.client.DoWithContext(ctx, req, &backupsMap)
+	if err != nil {
+		return nil, err
+	}
+
+	backup := new(Backup)
+	for _, bk := range backupsMap {
+		backup = &bk
+	}
+
+	return backup, nil
+}
+
+// GetListBySub retrieves a list of all backups on the current account that match the given subID
+func (b *BackupServiceHandler) GetListBySub(ctx context.Context, subID string) ([]Backup, error) {
+	uri := "/v1/backup/list"
+	req, err := b.client.NewRequest(ctx, http.MethodGet, uri, nil)
+
+	if err != nil {
+		return nil, err
+	}
+
+	q := req.URL.Query()
+	q.Add("SUBID", subID)
+	req.URL.RawQuery = q.Encode()
+
+	backupsMap := make(map[string]Backup)
 	err = b.client.DoWithContext(ctx, req, &backupsMap)
 	if err != nil {
 		return nil, err
--- backup_test.go
+++ backup_test.go
@@ -84,3 +84,143 @@ func TestBackupServiceHandler_GetListEmpty(t *testing.T) {
 		t.Errorf("Backup.GetList returned %+v, expected %+v", backups, expected)
 	}
 }
+
+func TestBackupServiceHandler_GetListBySub(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/v1/backup/list", func(w http.ResponseWriter, r *http.Request) {
+
+		response := `
+		{
+			"543d34149403a": {
+				"BACKUPID": "543d34149403a",
+				"date_created": "2014-10-14 12:40:40",
+				"description": "Automatic server backup",
+				"size": "42949672960",
+				"status": "complete"
+			},
+			"543d340f6dbce": {
+				"BACKUPID": "543d340f6dbce",
+				"date_created": "2014-10-13 16:11:46",
+				"description": "",
+				"size": "10000000",
+				"status": "complete"
+			}
+		}
+		`
+
+		fmt.Fprint(w, response)
+	})
+
+	backups, err := client.Backup.GetListBySub(ctx, "test-backupID")
+	if err != nil {
+		t.Errorf("Backup.GetListBySub returned error: %v", err)
+	}
+
+	expected := []Backup{
+		{
+			BackupID:    "543d34149403a",
+			DateCreated: "2014-10-14 12:40:40",
+			Description: "Automatic server backup",
+			Size:        "42949672960",
+			Status:      "complete",
+		},
+		{
+			BackupID:    "543d340f6dbce",
+			DateCreated: "2014-10-13 16:11:46",
+			Description: "",
+			Size:        "10000000",
+			Status:      "complete",
+		},
+	}
+
+	if !reflect.DeepEqual(backups, expected) {
+		t.Errorf("Backup.GetListBySub returned %+v, expected %+v", backups, expected)
+	}
+}
+
+func TestBackupServiceHandler_GetListBySubEmpty(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/v1/backup/list", func(w http.ResponseWriter, r *http.Request) {
+
+		response := `[]`
+
+		fmt.Fprint(w, response)
+	})
+
+	backups, err := client.Backup.GetListBySub(ctx, "test-backupID")
+	if err != nil {
+		t.Errorf("Backup.GetList returned error: %v", err)
+	}
+
+	var expected []Backup
+
+	if !reflect.DeepEqual(backups, expected) {
+		t.Errorf("Backup.GetList returned %+v, expected %+v", backups, expected)
+	}
+}
+
+func TestBackupServiceHandler_Get(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/v1/backup/list", func(w http.ResponseWriter, r *http.Request) {
+
+		response := `
+		{
+			"543d34149403a": {
+				"BACKUPID": "543d34149403a",
+				"date_created": "2014-10-14 12:40:40",
+				"description": "Automatic server backup",
+				"size": "42949672960",
+				"status": "complete"
+			}
+		}
+		`
+
+		fmt.Fprint(w, response)
+	})
+
+	backup, err := client.Backup.Get(ctx, "543d34149403a")
+	if err != nil {
+		t.Errorf("Backup.Get returned error: %v", err)
+	}
+
+	expected := &Backup{
+		BackupID:    "543d34149403a",
+		DateCreated: "2014-10-14 12:40:40",
+		Description: "Automatic server backup",
+		Size:        "42949672960",
+		Status:      "complete",
+	}
+
+	if !reflect.DeepEqual(backup, expected) {
+		t.Errorf("Backup.Get returned %+v, expected %+v", backup, expected)
+	}
+}
+
+func TestBackupServiceHandler_GetEmpty(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/v1/backup/list", func(w http.ResponseWriter, r *http.Request) {
+
+		response := `[]`
+
+		fmt.Fprint(w, response)
+	})
+
+	backup, err := client.Backup.Get(ctx, "test-backupID")
+	if err != nil {
+		t.Errorf("Backup.Get returned error: %v", err)
+	}
+
+	expected := &Backup{}
+
+	if !reflect.DeepEqual(backup, expected) {
+		t.Errorf("Backup.Get returned %+v, expected %+v", backup, expected)
+	}
+}
