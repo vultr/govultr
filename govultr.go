3b6772d99f1153e485906dc1ecab49865dec2127
Fady Farid <ffarid@vultr.com>
2019-04-26 09:11:08 -0400

    add startup script service and tests

--- govultr.go
+++ govultr.go
@@ -43,18 +43,19 @@ type Client struct {
 	RateLimit time.Duration
 
 	// Services used to interact with the API
-	Account      AccountService
-	API          APIService
-	Application  ApplicationService
-	Backup       BackupService
-	BlockStorage BlockStorageService
-	DNSDomain    DNSDomainService
-	DNSRecord    DNSRecordService
-	Iso          IsoService
-	OS           OSService
-	Plans        PlansService
-	Snapshot     SnapshotService
-	SSHKey       SSHKeyService
+	Account       AccountService
+	API           APIService
+	Application   ApplicationService
+	Backup        BackupService
+	BlockStorage  BlockStorageService
+	DNSDomain     DNSDomainService
+	DNSRecord     DNSRecordService
+	Iso           IsoService
+	OS            OSService
+	Plans         PlansService
+	Snapshot      SnapshotService
+	SSHKey        SSHKeyService
+	StartupScript StartupScriptService
 
 	// Optional function called after every successful request made to the Vultr API
 	onRequestCompleted RequestCompletionCallback
@@ -91,6 +92,7 @@ func NewClient(httpClient *http.Client, key string) *Client {
 	client.Plans = &PlansServiceHandler{client}
 	client.Snapshot = &SnapshotServiceHandler{client}
 	client.SSHKey = &SSHKeyServiceHandler{client}
+	client.StartupScript = &StartupScriptServiceHandler{client}
 
 	apiKey := APIKey{key: key}
 	client.APIKey = apiKey
---
+++ startup_script_test.go
@@ -0,0 +1,137 @@
+package govultr
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"reflect"
+	"testing"
+)
+
+func TestStartupScriptServiceHandler_Create(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/v1/startupscript/create", func(writer http.ResponseWriter, request *http.Request) {
+		response := `
+		{
+			"SCRIPTID": 5
+		}
+		`
+
+		fmt.Fprint(writer, response)
+	})
+
+	s, err := client.StartupScript.Create(context.Background(), "foo", "#!/bin/bash\necho hello world > /root/hello", "pxe")
+
+	if err != nil {
+		t.Errorf("StartupScript.Create returned %+v, expected %+v", err, nil)
+	}
+
+	expected := &StartupScript{
+		ScriptID: "5",
+		Name:     "foo",
+		Type:     "pxe",
+		Script:   "#!/bin/bash\necho hello world > /root/hello",
+	}
+
+	if !reflect.DeepEqual(s, expected) {
+		t.Errorf("StartupScript.Create returned %+v, expected %+v", s, expected)
+	}
+}
+
+func TestStartupScriptServiceHandler_Destroy(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/v1/startupscript/destroy", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer)
+	})
+
+	err := client.StartupScript.Destroy(ctx, "foo")
+
+	if err != nil {
+		t.Errorf("StartupScript.Destroy returned %+v, expected %+v", err, nil)
+	}
+}
+
+func TestStartupScriptServiceHandler_GetList(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/v1/startupscript/list", func(writer http.ResponseWriter, request *http.Request) {
+		response := `
+		{
+			"3": {
+				"SCRIPTID": "3",
+				"date_created": "2014-05-21 15:27:18",
+				"date_modified": "2014-05-21 15:27:18",
+				"name": "foo",
+				"type": "boot",
+				"script": "#!/bin/bash echo Hello World > /root/hello"
+			},
+			"5": {
+				"SCRIPTID": "5",
+				"date_created": "2014-08-22 15:27:18",
+				"date_modified": "2014-09-22 15:27:18",
+				"name": "bar",
+				"type": "pxe",
+				"script": "#!ipxe\necho Hello World\nshell"
+			}
+		}
+		`
+		fmt.Fprintf(writer, response)
+	})
+
+	scripts, err := client.StartupScript.GetList(ctx)
+
+	if err != nil {
+		t.Errorf("StartupScript.GetList returned error: %v", err)
+	}
+
+	expected := []StartupScript{
+		{
+			ScriptID:     "3",
+			Name:         "foo",
+			Type:         "boot",
+			Script:       "#!/bin/bash echo Hello World > /root/hello",
+			DateCreated:  "2014-05-21 15:27:18",
+			DateModified: "2014-05-21 15:27:18",
+		},
+		{
+			ScriptID:     "5",
+			Name:         "bar",
+			Type:         "pxe",
+			Script:       "#!ipxe\necho Hello World\nshell",
+			DateCreated:  "2014-08-22 15:27:18",
+			DateModified: "2014-09-22 15:27:18",
+		},
+	}
+
+	if !reflect.DeepEqual(scripts, expected) {
+		t.Errorf("StartupScript.GetList returned %+v, expected %+v", scripts, expected)
+	}
+}
+
+func TestStartupScriptServiceHandler_Update(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/v1/startupscript/update", func(writer http.ResponseWriter, request *http.Request) {
+
+		fmt.Fprint(writer)
+	})
+
+	script := &StartupScript{
+		ScriptID: "1",
+		Name:     "foo",
+		Type:     "boot",
+		Script:   "#!/bin/bash echo Hello World > /root/hello",
+	}
+
+	err := client.StartupScript.Update(ctx, script)
+
+	if err != nil {
+		t.Errorf("StartupScript.Update returned error: %+v", err)
+	}
+}
