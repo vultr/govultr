ada5885d7fd841b5a213fff8d03520397dbe62c1
Mariah McLaughlin <mclaugm3@tcnj.edu>
2020-07-15 21:15:51 -0400

    update v2 networks

--- network.go
+++ network.go
@@ -2,18 +2,21 @@ package govultr
 
 import (
 	"context"
-	"net"
+	"fmt"
 	"net/http"
-	"net/url"
-	"strconv"
+
+	"github.com/google/go-querystring/query"
 )
 
+const netPath = "/v2/networks"
+
 // NetworkService is the interface to interact with the network endpoints on the Vultr API
-// Link: https://www.vultr.com/api/#network
 type NetworkService interface {
-	Create(ctx context.Context, regionID, description, cidrBlock string) (*Network, error)
+	Create(ctx context.Context, createReq *NetworkReq) (*Network, error)
+	Get(ctx context.Context, networkID string) (*Network, error)
+	Update(ctx context.Context, networkID string, updateReq *NetworkReq) error
 	Delete(ctx context.Context, networkID string) error
-	List(ctx context.Context) ([]Network, error)
+	List(ctx context.Context, options *ListOptions) ([]Network, *Meta, error)
 }
 
 // NetworkServiceHandler handles interaction with the network methods for the Vultr API
@@ -23,73 +26,86 @@ type NetworkServiceHandler struct {
 
 // Network represents a Vultr private network
 type Network struct {
-	NetworkID    string `json:"NETWORKID"`
-	RegionID     string `json:"DCID"`
+	NetworkID    string `json:"id"`
+	Region       string `json:"region"`
 	Description  string `json:"description"`
 	V4Subnet     string `json:"v4_subnet"`
 	V4SubnetMask int    `json:"v4_subnet_mask"`
 	DateCreated  string `json:"date_created"`
 }
 
-// Create a new private network. A private network can only be used at the location for which it was created.
-func (n *NetworkServiceHandler) Create(ctx context.Context, regionID, description, cidrBlock string) (*Network, error) {
-
-	uri := "/v1/network/create"
-
-	values := url.Values{
-		"DCID": {regionID},
-	}
-
-	// Optional
-	if cidrBlock != "" {
-		_, ipNet, err := net.ParseCIDR(cidrBlock)
-		if err != nil {
-			return nil, err
-		}
-		if v4Subnet := ipNet.IP.To4(); v4Subnet != nil {
-			values.Add("v4_subnet", v4Subnet.String())
-		}
-		mask, _ := ipNet.Mask.Size()
-		values.Add("v4_subnet_mask", strconv.Itoa(mask))
-	}
-
-	if description != "" {
-		values.Add("description", description)
-	}
-
-	req, err := n.client.NewRequest(ctx, http.MethodPost, uri, values)
-
-	if err != nil {
-		return nil, err
-	}
-
-	network := new(Network)
-	err = n.client.DoWithContext(ctx, req, network)
-
-	if err != nil {
-		return nil, err
-	}
-
-	return network, nil
+// NetworkReq represents parameters to create or update Network resource
+type NetworkReq struct {
+	Region       string `json:"region"`
+	Description  string `json:"description"`
+	V4Subnet     string `json:"v4_subnet"`
+	V4SubnetMask int    `json:"v4_subnet_mask"`
 }
 
-// Delete a private network. Before deleting, a network must be disabled from all instances. See https://www.vultr.com/api/#server_private_network_disable
-func (n *NetworkServiceHandler) Delete(ctx context.Context, networkID string) error {
-	uri := "/v1/network/destroy"
+type networksBase struct {
+	Networks []Network `json:"networks"`
+	Meta     *Meta     `json:"meta"`
+}
 
-	values := url.Values{
-		"NETWORKID": {networkID},
+type networkBase struct {
+	Network *Network `json:"network"`
+}
+
+// Create a new private network. A private network can only be used at the location for which it was created.
+func (n *NetworkServiceHandler) Create(ctx context.Context, createReq *NetworkReq) (*Network, error) {
+	req, err := n.client.NewRequest(ctx, http.MethodPost, netPath, createReq)
+	if err != nil {
+		return nil, err
 	}
 
-	req, err := n.client.NewRequest(ctx, http.MethodPost, uri, values)
+	network := new(networkBase)
+	if err = n.client.DoWithContext(ctx, req, network); err != nil {
+		return nil, err
+	}
 
+	return network.Network, nil
+}
+
+// Get gets the private networks of the requested ID
+func (n *NetworkServiceHandler) Get(ctx context.Context, networkID string) (*Network, error) {
+	uri := fmt.Sprintf("%s/%s", netPath, networkID)
+	req, err := n.client.NewRequest(ctx, http.MethodGet, uri, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	network := new(networkBase)
+	if err = n.client.DoWithContext(ctx, req, network); err != nil {
+		return nil, err
+	}
+
+	return network.Network, nil
+}
+
+// Update updates a private network
+func (n *NetworkServiceHandler) Update(ctx context.Context, networkID string, updateReq *NetworkReq) error {
+	uri := fmt.Sprintf("%s/%s", netPath, networkID)
+	req, err := n.client.NewRequest(ctx, http.MethodPut, uri, updateReq)
 	if err != nil {
 		return err
 	}
 
-	err = n.client.DoWithContext(ctx, req, nil)
+	if err = n.client.DoWithContext(ctx, req, nil); err != nil {
+		return err
+	}
 
+	return nil
+}
+
+// Delete a private network. Before deleting, a network must be disabled from all instances. See https://www.vultr.com/api/#server_private_network_disable
+func (n *NetworkServiceHandler) Delete(ctx context.Context, networkID string) error {
+	uri := fmt.Sprintf("%s/%s", netPath, networkID)
+	req, err := n.client.NewRequest(ctx, http.MethodDelete, uri, nil)
 	if err != nil {
+		return err
+	}
+
+	if err = n.client.DoWithContext(ctx, req, nil); err != nil {
 		return err
 	}
 
@@ -97,24 +113,23 @@ func (n *NetworkServiceHandler) Delete(ctx context.Context, networkID string) er
 }
 
 // List lists all private networks on the current account
-func (n *NetworkServiceHandler) List(ctx context.Context) ([]Network, error) {
-	uri := "/v1/network/list"
-
-	req, err := n.client.NewRequest(ctx, http.MethodGet, uri, nil)
+func (n *NetworkServiceHandler) List(ctx context.Context, options *ListOptions) ([]Network, *Meta, error) {
+	req, err := n.client.NewRequest(ctx, http.MethodGet, netPath, nil)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
-	var networkMap map[string]Network
-	err = n.client.DoWithContext(ctx, req, &networkMap)
+	newValues, err := query.Values(options)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
-	var networks []Network
-	for _, network := range networkMap {
-		networks = append(networks, network)
+	req.URL.RawQuery = newValues.Encode()
+
+	networks := new(networksBase)
+	if err = n.client.DoWithContext(ctx, req, networks); err != nil {
+		return nil, nil, err
 	}
 
-	return networks, nil
+	return networks.Networks, networks.Meta, nil
 }
