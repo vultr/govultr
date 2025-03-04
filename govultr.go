9332f515d8da7b5d1d80f109a0b16bb53fd41ac6
Michael Riley <mriley@vultr.com>
2025-03-04 14:29:53 -0500

    Release v3.15.0 #minor

--- govultr.go
+++ govultr.go
@@ -19,7 +19,7 @@ import (
 )
 
 const (
-	version     = "3.14.1"
+	version     = "3.15.0"
 	defaultBase = "https://api.vultr.com"
 	userAgent   = "govultr/" + version
 	rateLimit   = 500 * time.Millisecond
