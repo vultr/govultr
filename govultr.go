52752378ae2ff8d03ba08be1d5be0611f3ced95c
Michael Riley <mriley@vultr.com>
2025-03-28 09:36:03 -0400

    Increase default timeouts

--- govultr.go
+++ govultr.go
@@ -88,18 +88,18 @@ func NewClient(httpClient *http.Client) *Client {
 		httpClient = &http.Client{
 			Transport: &http.Transport{
 				DialContext: (&net.Dialer{
-					Timeout:   30 * time.Second,
-					KeepAlive: 30 * time.Second,
+					Timeout:   90 * time.Second,
+					KeepAlive: 90 * time.Second,
 					DualStack: true,
 				}).DialContext,
 				MaxIdleConns:          100,
 				IdleConnTimeout:       90 * time.Second,
-				TLSHandshakeTimeout:   10 * time.Second,
+				TLSHandshakeTimeout:   30 * time.Second,
 				ExpectContinueTimeout: 1 * time.Second,
 				MaxIdleConnsPerHost:   -1,
 				DisableKeepAlives:     true,
 			},
-			Timeout: 5 * time.Second,
+			Timeout: 30 * time.Second,
 		}
 	}
 
