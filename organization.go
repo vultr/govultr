359a11faebde7e1e1c0e358d6824e6248ac21c79
Michael Riley <mriley@vultr.com>
2025-12-05 13:33:25 -0500

    Omit empty role trust update fields

--- organization.go
+++ organization.go
@@ -518,9 +518,9 @@ type OrganizationRoleTrustReq struct {
 	RoleID      string                         `json:"role_id,omitempty"`
 	UserID      string                         `json:"trusted_user_id,omitempty"`
 	GroupID     string                         `json:"trusted_group_id,omitempty"`
-	Type        string                         `json:"trust_type"`
-	Conditions  OrganizationRoleTrustCondition `json:"conditions"`
-	DateExpires string                         `json:"valid_until"`
+	Type        string                         `json:"trust_type,omitempty"`
+	Conditions  OrganizationRoleTrustCondition `json:"conditions,omitempty"`
+	DateExpires string                         `json:"valid_until,omitempty"`
 }
 
 // OrganizationRoleAssumedReq represents an organization assumed role
