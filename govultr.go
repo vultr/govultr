8265a8d1e352ccb605450a1bdb3c4bab29af310a
Michael Riley <mriley@vultr.com>
2026-02-25 15:29:46 -0500

    Add OIDC (#439)

--- govultr.go
+++ govultr.go
@@ -61,6 +61,7 @@ type Client struct {
 	Logs                     LogsService
 	Marketplace              MarketplaceService
 	ObjectStorage            ObjectStorageService
+	OIDC                     OIDCService
 	Organization             OrganizationService
 	OS                       OSService
 	Plan                     PlanService
@@ -139,6 +140,7 @@ func NewClient(httpClient *http.Client) *Client {
 	client.Logs = &LogsServiceHandler{client}
 	client.Marketplace = &MarketplaceServiceHandler{client}
 	client.ObjectStorage = &ObjectStorageServiceHandler{client}
+	client.OIDC = &OIDCServiceHandler{client}
 	client.Organization = &OrganizationServiceHandler{client}
 	client.OS = &OSServiceHandler{client}
 	client.Plan = &PlanServiceHandler{client}
