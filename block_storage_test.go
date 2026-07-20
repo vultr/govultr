f3c12408c30e59e3e30d96b076527d4da2468990
Michael Riley <mriley@vultr.com>
2026-07-20 17:38:00 -0400

    Add block storage snapshot tests

--- block_storage_test.go
+++ block_storage_test.go
@@ -202,3 +202,208 @@ func TestBlockStorageServiceHandler_Detach(t *testing.T) {
 		t.Errorf("BlockStorage.Detach returned %+v, expected %+v", err, nil)
 	}
 }
+
+func TestBlockStorageServiceHandler_ListSnapshots(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/v2/blocks/snapshots", func(writer http.ResponseWriter, request *http.Request) {
+		response := `{
+	"snapshots": [
+		{
+			"id": "cb676a46-66fd-4dfb-b839-443f2e6c0b60",
+			"block_id": "ay2c5b1a-66fd-4dfb-b839-443f2e6c0b60",
+			"description": "block storage snapshot",
+			"state": "COMPLETE",
+			"added_at": "2026-07-17 16:46:05",
+			"updated_at": "2026-07-17 16:46:09",
+			"size": 10737418240,
+			"next_invoice_date": "",
+			"next_invoice_price": ""
+		},
+		{
+			"id": "96aef714-ce33-46a7-88e1-2eb1e8e0646e",
+			"description": "another snapshot test",
+			"block_id": "f004aea7-aec2-47fe-9ad7-7ebd830f225e",
+			"state": "COMPLETE",
+			"added_at": "2026-07-17 16:46:05",
+			"updated_at": "2026-07-17 16:46:09",
+			"next_invoice_date": "",
+			"next_invoice_price": "",
+			"size": 107374182400
+		}
+	],
+		"meta": {
+			"total": 2,
+			"links": {
+				"next": "",
+				"prev": ""
+		}
+	}
+}`
+		fmt.Fprint(writer, response)
+	})
+
+	snapshots, meta, _, err := client.BlockStorage.ListSnapshots(ctx, nil)
+	if err != nil {
+		t.Errorf("BlockStorage.ListSnapshots returned error: %v", err)
+	}
+
+	expected := []BlockStorageSnapshot{
+		{
+			ID:               "cb676a46-66fd-4dfb-b839-443f2e6c0b60",
+			BlockID:          "ay2c5b1a-66fd-4dfb-b839-443f2e6c0b60",
+			Description:      "block storage snapshot",
+			State:            "COMPLETE",
+			DateCreated:      "2026-07-17 16:46:05",
+			DateUpdated:      "2026-07-17 16:46:09",
+			InvoiceNextDate:  "",
+			InvoiceNextPrice: "",
+			Size:             10737418240,
+		},
+		{
+			ID:               "96aef714-ce33-46a7-88e1-2eb1e8e0646e",
+			Description:      "another snapshot test",
+			BlockID:          "f004aea7-aec2-47fe-9ad7-7ebd830f225e",
+			State:            "COMPLETE",
+			DateCreated:      "2026-07-17 16:46:05",
+			DateUpdated:      "2026-07-17 16:46:09",
+			InvoiceNextDate:  "",
+			InvoiceNextPrice: "",
+			Size:             107374182400,
+		},
+	}
+
+	if !reflect.DeepEqual(snapshots, expected) {
+		t.Errorf("BlockStorage.ListSnapshots returned %+v, expected %+v", snapshots, expected)
+	}
+
+	expectedMeta := &Meta{
+		Total: 2,
+		Links: &Links{
+			Next: "",
+			Prev: "",
+		},
+	}
+
+	if !reflect.DeepEqual(meta, expectedMeta) {
+		t.Errorf("BlockStorage.ListSnapshots meta returned %+v, expected %+v", meta, expectedMeta)
+	}
+}
+
+func TestBlockStorageServiceHandler_GetSnapshot(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/v2/blocks/snapshots/cb676a46-66fd-4dfb-b839-443f2e6c0b60", func(writer http.ResponseWriter, request *http.Request) {
+		response := `{
+	"id": "cb676a46-66fd-4dfb-b839-443f2e6c0b60",
+	"block_id": "ay2c5b1a-66fd-4dfb-b839-443f2e6c0b60",
+	"description": "block storage snapshot",
+	"state": "COMPLETE",
+	"added_at": "2026-07-17 16:46:05",
+	"updated_at": "2026-07-17 16:46:09",
+	"size": 10737418240,
+	"next_invoice_date": "",
+	"next_invoice_price": ""
+}
+`
+		fmt.Fprint(writer, response)
+	})
+
+	snapshot, _, err := client.BlockStorage.GetSnapshot(ctx, "cb676a46-66fd-4dfb-b839-443f2e6c0b60")
+	if err != nil {
+		t.Errorf("BlockStorage.GetSnapshot returned error: %v", err)
+	}
+
+	expected := &BlockStorageSnapshot{
+		ID:               "cb676a46-66fd-4dfb-b839-443f2e6c0b60",
+		BlockID:          "ay2c5b1a-66fd-4dfb-b839-443f2e6c0b60",
+		Description:      "block storage snapshot",
+		State:            "COMPLETE",
+		DateCreated:      "2026-07-17 16:46:05",
+		DateUpdated:      "2026-07-17 16:46:09",
+		InvoiceNextDate:  "",
+		InvoiceNextPrice: "",
+		Size:             10737418240,
+	}
+
+	if !reflect.DeepEqual(snapshot, expected) {
+		t.Errorf("BlockStorage.GetSnapshot returned %+v, expected %+v", snapshot, expected)
+	}
+}
+
+func TestBlockStorageServiceHandler_CreateSnapshot(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/v2/blocks/snapshots", func(writer http.ResponseWriter, request *http.Request) {
+		response := `{
+	"id": "cb676a46-66fd-4dfb-b839-443f2e6c0b60",
+	"block_id": "ay2c5b1a-66fd-4dfb-b839-443f2e6c0b60",
+	"description": "block storage snapshot",
+	"state": "COMPLETE",
+	"added_at": "2026-07-17 16:46:05",
+	"updated_at": "2026-07-17 16:46:09",
+	"size": 10737418240,
+	"next_invoice_date": "",
+	"next_invoice_price": ""
+}
+`
+		fmt.Fprint(writer, response)
+	})
+	snapshotReq := &BlockStorageSnapshotReq{
+		BlockID:     "ay2c5b1a-66fd-4dfb-b839-443f2e6c0b60",
+		Description: "block storage snapshot",
+	}
+	snapshot, _, err := client.BlockStorage.CreateSnapshot(ctx, snapshotReq)
+	if err != nil {
+		t.Errorf("BlockStorage.CreateSnapshot returned error: %v", err)
+	}
+
+	expected := &BlockStorageSnapshot{
+		ID:               "cb676a46-66fd-4dfb-b839-443f2e6c0b60",
+		BlockID:          "ay2c5b1a-66fd-4dfb-b839-443f2e6c0b60",
+		Description:      "block storage snapshot",
+		State:            "COMPLETE",
+		DateCreated:      "2026-07-17 16:46:05",
+		DateUpdated:      "2026-07-17 16:46:09",
+		InvoiceNextDate:  "",
+		InvoiceNextPrice: "",
+		Size:             10737418240,
+	}
+
+	if !reflect.DeepEqual(snapshot, expected) {
+		t.Errorf("BlockStorage.CreateSnapshot returned %+v, expected %+v", snapshot, expected)
+	}
+}
+
+func TestBlockStorageServiceHandler_UpdateSnapshot(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/v2/blocks/snapshots/cb676a46-66fd-4dfb-b839-443f2e6c0b60", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer)
+	})
+
+	snapshotReq := &BlockStorageSnapshotReq{
+		Description: "block storage snapshot updated",
+	}
+
+	if err := client.BlockStorage.UpdateSnapshot(ctx, "cb676a46-66fd-4dfb-b839-443f2e6c0b60", snapshotReq); err != nil {
+		t.Errorf("BlockStorage.UpdateSnapshot returned %+v, expected %+v", err, nil)
+	}
+}
+
+func TestBlockStorageServiceHandler_DeleteSnapshot(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/v2/blocks/snapshots/cb676a46-66fd-4dfb-b839-443f2e6c0b60", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer)
+	})
+
+	if err := client.BlockStorage.DeleteSnapshot(ctx, "cb676a46-66fd-4dfb-b839-443f2e6c0b60"); err != nil {
+		t.Errorf("BlockStorage.DeleteSnapshot returned %+v, expected %+v", err, nil)
+	}
+}
